97be93871eb8dfcf1211074de2477b5f72b2fa7a
JpabloMF <juanpablo45671@gmail.com>
2019-07-06 17:59:44 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "react-app", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-console": "warn",
+    "no-unneeded-ternary": ["error", true],
+    "prettier/prettier": "error"
+  }
+};
