c5298e864c303fe196dd041a305237e7c54a05e7
JpabloMF <juanpablo45671@gmail.com>
2019-07-18 21:06:20 -0500

    adding currencies module

---
+++ src/screens/currencies/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Curriencies = () => {
+  return <div>Curriencies</div>;
+};
+
+Curriencies.propTypes = {
+  props: PropTypes.object.isRequired
+};
+
+export default Curriencies;
